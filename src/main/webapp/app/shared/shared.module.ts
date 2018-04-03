253ac3f35216b6a609235b543a2126ca9a54b6e8
derekzuk <derekzuk@gmail.com>
2018-04-02 20:13:46 -0400

    Initial application generated by JHipster-4.14.0

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,49 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import {
+    FauxshopangularSharedLibsModule,
+    FauxshopangularSharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    JhiLoginModalComponent,
+    Principal,
+    HasAnyAuthorityDirective,
+} from './';
+
+@NgModule({
+    imports: [
+        FauxshopangularSharedLibsModule,
+        FauxshopangularSharedCommonModule
+    ],
+    declarations: [
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        AuthServerProvider,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        FauxshopangularSharedCommonModule,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class FauxshopangularSharedModule {}

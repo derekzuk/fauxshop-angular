7ac8edec374f887349f6303a6a7b6310594235f8
derekzuk <derekzuk@gmail.com>
2018-04-17 13:46:10 -0400

    Updated home page to show cart properly

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
-
+import { Cart } from '../shared/shop/cart.model';
+import { CartService } from '../shared/shop/cart.service';
 import { Account, LoginModalService, Principal } from '../shared';
 
 @Component({
@@ -15,9 +16,11 @@ import { Account, LoginModalService, Principal } from '../shared';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
+    cart: Cart[] = [];
 
     constructor(
         private principal: Principal,
+        private cartService: CartService,
         private loginModalService: LoginModalService,
         private eventManager: JhiEventManager
     ) {
@@ -26,6 +29,7 @@ export class HomeComponent implements OnInit {
     ngOnInit() {
         this.principal.identity().then((account) => {
             this.account = account;
+            this.updateCart();
         });
         this.registerAuthenticationSuccess();
     }
@@ -34,12 +38,21 @@ export class HomeComponent implements OnInit {
         this.eventManager.subscribe('authenticationSuccess', (message) => {
             this.principal.identity().then((account) => {
                 this.account = account;
+                this.updateCart();
             });
         });
     }
 
     isAuthenticated() {
         return this.principal.isAuthenticated();
+    }
+
+    updateCart() {
+        if (this.account != null) {
+            this.cartService.getCartByUserId(this.account.id).subscribe((cartData) => {
+                this.cart = cartData;
+            });
+        }
     }
 
     login() {
